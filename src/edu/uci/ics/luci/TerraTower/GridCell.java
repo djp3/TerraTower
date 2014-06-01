701136d1b8ee90271c685e077626fa80ec4f9005
Don Patterson <d_j_p_3@djp3.net>
2014-05-31 18:18:58 -0700

    Added a GridCell test

--- src/edu/uci/ics/luci/TerraTower/GridCell.java
+++ src/edu/uci/ics/luci/TerraTower/GridCell.java
@@ -17,30 +17,18 @@ public class GridCell {
 	private int x;
 	private int y;
 	
-	/**
-	 * @return the x
-	 */
 	public int getX() {
 		return x;
 	}
 
-	/**
-	 * @param x the x to set
-	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
-	/**
-	 * @return the y
-	 */
 	public int getY() {
 		return y;
 	}
 
-	/**
-	 * @param y the y to set
-	 */
 	public void setY(int y) {
 		this.y = y;
 	}
---
+++ src_test/edu/uci/ics/luci/TerraTower/GridCellTest.java
@@ -0,0 +1,38 @@
+package edu.uci.ics.luci.TerraTower;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GridCellTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		int x = 10;
+		int y = 10;
+		GridCell gc = new GridCell(x,y);
+		assertEquals(x,gc.getX());
+		assertEquals(y,gc.getY());
+	}
+
+}
