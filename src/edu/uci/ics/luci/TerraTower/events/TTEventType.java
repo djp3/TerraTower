a2190254d51da9e9f39ce63209c68f21134872d2
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 15:14:37 -0700

    Cleaning up post place tower event

--- src/edu/uci/ics/luci/TerraTower/events/TTEventType.java
+++ src/edu/uci/ics/luci/TerraTower/events/TTEventType.java
@@ -3,18 +3,18 @@ package edu.uci.ics.luci.TerraTower.events;
 public enum TTEventType{
 	VOID,
 	CREATE_WORLD,
-	CREATE_MAP,
+	CREATE_TERRITORY,
 	CREATE_PLAYER,
-	PLACE_TOWER,
-	STEP_TOWER_TERRITORY_GROWTH;
+	PLACE_TOWER;
+	//STEP_TOWER_TERRITORY_GROWTH;
 	
 	public static String toString(TTEventType x){
 		switch (x){
 			case CREATE_WORLD: return "CREATE_WORLD";
-			case CREATE_MAP: return "CREATE_MAP";
+			case CREATE_TERRITORY: return "CREATE_TERRITORY";
 			case CREATE_PLAYER: return "CREATE_PLAYER";
 			case PLACE_TOWER: return "PLACE_TOWER";
-			case STEP_TOWER_TERRITORY_GROWTH: return "STEP_TOWER_TERRITORY_GROWTH";
+			//case STEP_TOWER_TERRITORY_GROWTH: return "STEP_TOWER_TERRITORY_GROWTH";
 			default: return "VOID";
 		}
 	}
@@ -22,10 +22,10 @@ public enum TTEventType{
 	static public TTEventType fromString(String x){
 		switch (x){
 			case "CREATE_WORLD": return CREATE_WORLD;
-			case "CREATE_MAP": return CREATE_MAP;
+			case "CREATE_TERRITORY": return CREATE_TERRITORY;
 			case "CREATE_PLAYER": return CREATE_PLAYER;
 			case "PLACE_TOWER": return PLACE_TOWER;
-			case "STEP_TOWER_TERRITORY_GROWTH": return STEP_TOWER_TERRITORY_GROWTH;
+			//case "STEP_TOWER_TERRITORY_GROWTH": return STEP_TOWER_TERRITORY_GROWTH;
 			default: return VOID;
 		}
 	}
