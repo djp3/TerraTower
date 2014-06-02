6f0ce869121dfa98c574c9e1c0f7319f12505ef1
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 16:30:10 -0700

    Cleaning up post place tower event

--- src/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreatePlayer.java
+++ src/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreatePlayer.java
@@ -1,53 +1,105 @@
-package edu.uci.ics.luci.TerraTower.gameEvents;
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
+
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.uci.ics.luci.TerraTower.events.handlers;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import edu.uci.ics.luci.TerraTower.GlobalsTerraTower;
-import edu.uci.ics.luci.TerraTower.TTEvent;
-import edu.uci.ics.luci.TerraTower.TTEventHandler;
-import edu.uci.ics.luci.TerraTower.world.WorldManager;
+import edu.uci.ics.luci.TerraTower.events.TTEvent;
+import edu.uci.ics.luci.TerraTower.events.TTEventCreatePlayer;
 
 
-public class TTEventHandlerCreatePlayer implements TTEventHandler{    
+public class TTEventHandlerCreatePlayer extends TTEventHandler{    
 	
-	@Override
-	public JSONObject onEvent(TTEvent _event) {
-		JSONObject ret = new JSONObject();
-		TTEventCreatePlayer event = (TTEventCreatePlayer) _event;
-		WorldManager wm = GlobalsTerraTower.getGlobalsTerraTower().getWorldManager();
-		if(wm.playerExists(event.getPlayerName())){
-			ret.put("error","true");
-			JSONArray errors = new JSONArray();
-			errors.add("Player with name, "+event.getPlayerName()+" already exists");
-			ret.put("errors", errors);
-		}else{
-			if(wm.createPlayer(event.getPlayerName(),event.getHashedPassword())){
-				ret.put("error", "false");
-			}
-			else{
-				ret.put("error","true");
-				JSONArray errors = new JSONArray();
-				errors.add("Player with name, "+event.getPlayerName()+" could not be created");
-				ret.put("errors", errors);
-			}
-		}
-		return ret;
-	}
+
+	private String playerName;
+	private byte[] playerHashedPassword;
 
 	@Override
-	public JSONObject checkParameters(TTEvent _event) {
-		JSONObject ret = new JSONObject();
-		TTEventCreatePlayer event = (TTEventCreatePlayer) _event;
-		
-		if(event.getPlayerName() == null){
+	public JSONObject checkParameters(long eventTime,TTEvent _event) {
+		//Check parent 
+		JSONObject ret = super.checkParameters(eventTime, _event);
+		if(ret != null){
+			return ret;
+		}
+		ret = new JSONObject();
+				
+		TTEventCreatePlayer event = null;
+		if(_event instanceof TTEventCreatePlayer){
+			event = ((TTEventCreatePlayer) _event);
+		}
+		else{
 			ret.put("error","true");
 			JSONArray errors = new JSONArray();
-			errors.add("Player can't have a null name");
+			errors.add("Internal error, event type mismatch\n"+this.getClass().getCanonicalName()+" was called with "+_event.getClass().getCanonicalName());
+			ret.put("errors", errors);
+			return ret;
+		}
+
+		//Does player name exist?
+		playerName = event.getPlayerName();
+		if(playerName == null){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Playername was null");
+			ret.put("errors", errors);
+			return ret;
+		}
+
+		//Does player password exist?
+		playerHashedPassword = event.getPlayerHashedPassword();
+		/*We know it can't be null*/
+		/*
+		if(playerHashedPassword == null){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Player password was null");
+			ret.put("errors", errors);
+			return ret;
+		}*/
+		
+		//Does player already exist?
+		if(wm.playerExists(playerName)){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Player already exists can't creat them");
 			ret.put("errors", errors);
 			return ret;
 		}
 		
-		
 		return null;
 	}
+	
+	@Override
+	public JSONObject onEvent() {
+		JSONObject ret = new JSONObject();
+		if(!wm.createPlayer(playerName,playerHashedPassword)){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Could not create player for unknown reason");
+			ret.put("errors", errors);
+		}
+		else{
+			ret.put("error", "false");
+		}
+		return ret;
+	}
+
 }
--- src_test/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreatePlayerTest.java
+++ src_test/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreatePlayerTest.java
@@ -1,4 +1,24 @@
-package edu.uci.ics.luci.TerraTower.gameEvents;
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
+
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.uci.ics.luci.TerraTower.events.handlers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -11,10 +31,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.uci.ics.luci.TerraTower.GlobalsTerraTower;
-import edu.uci.ics.luci.TerraTower.world.WorldManager;
+import edu.uci.ics.luci.TerraTower.events.TTEvent;
+import edu.uci.ics.luci.TerraTower.events.TTEventCreatePlayer;
 import edu.uci.ics.luci.utility.Globals;
 
 public class TTEventHandlerCreatePlayerTest {
+	
+	static final String worldName = "name"+System.currentTimeMillis();
+	static final String worldPassword = "password"+System.currentTimeMillis();
+	static final String playerName = "name"+System.currentTimeMillis();
+	static final String playerPassword = "password"+System.currentTimeMillis();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -35,39 +61,71 @@ public class TTEventHandlerCreatePlayerTest {
 	@Test
 	public void testParameters() {
 		TTEventHandlerCreatePlayer tt = new TTEventHandlerCreatePlayer();
-		String playerName = "player name";
-		String password = "password";
-		TTEventCreatePlayer event = new TTEventCreatePlayer(playerName,password);
 		
-		JSONObject json = tt.checkParameters(event);
-		assertTrue(json == null);
-		
-		event = new TTEventCreatePlayer(null,password);
-		json = tt.checkParameters(event);
+		/* Super type catches lack of globals error*/
+		TTEvent tx = new TTEvent(worldName,worldPassword);
+		JSONObject json = tt.checkParameters(0,tx);
 		assertTrue(json != null);
 		assertEquals("true",(String)json.get("error"));
 		
-		event = new TTEventCreatePlayer(playerName,(String)null);
-		json = tt.checkParameters(event);
+		/* Wrong type*/
+		GlobalsTerraTower g = new GlobalsTerraTower("Version test");
+		Globals.setGlobals(g);
+		g.createWorld(worldName, worldPassword);
+		
+		tx = new TTEvent(worldName,worldPassword);
+		json = tt.checkParameters(0,tx);
+		assertTrue(json != null);
+		assertEquals("true",(String)json.get("error"));
+		
+		/* null player */
+		//tx = new TTEventCreatePlayer(worldName,worldPassword,playerName,playerPassword);
+		tx = new TTEventCreatePlayer(worldName,worldPassword,null,playerPassword);
+		json = tt.checkParameters(0,tx);
+		assertTrue(json != null);
+		assertEquals("true",(String)json.get("error"));
+		
+		/* Ok! */
+		tx = new TTEventCreatePlayer(worldName,worldPassword,playerName,playerPassword);
+		json = tt.checkParameters(0,tx);
 		assertTrue(json == null);
+		
+		//Execute creation
+		json = tt.onEvent();
+		assertTrue(json != null);
+		assertEquals("false",(String)json.get("error"));
+		
+		/* Fail parameters because player exists */
+		tx = new TTEventCreatePlayer(worldName,worldPassword,playerName,playerPassword);
+		json = tt.checkParameters(0,tx);
+		assertTrue(json != null);
+		assertEquals("true",(String)json.get("error"));
+		
+		//Fail to Execute creation because player exists
+		json = tt.onEvent();
+		assertTrue(json != null);
+		assertEquals("true",(String)json.get("error"));
 	}
 	
 	@Test
 	public void testOnEvent() {
 		GlobalsTerraTower g = new GlobalsTerraTower("Version test");
 		Globals.setGlobals(g);
-		g.setWorldManager(new WorldManager());
-		
+		g.createWorld(worldName, worldPassword);
+
+		/* Ok! */
 		TTEventHandlerCreatePlayer tt = new TTEventHandlerCreatePlayer();
-		String playerName = "player name";
-		String password = "password";
-		TTEventCreatePlayer event = new TTEventCreatePlayer(playerName,password);
+		TTEventCreatePlayer tx = new TTEventCreatePlayer(worldName,worldPassword,playerName,playerPassword);
+		JSONObject json = tt.checkParameters(0,tx);
+		assertTrue(json == null);
 		
-		JSONObject json = tt.onEvent(event);
+		//Execute creation
+		json = tt.onEvent();
 		assertTrue(json != null);
 		assertEquals("false",(String)json.get("error"));
-		
-		json = tt.onEvent(event);
+				
+		//Fail to Execute creation because player exists
+		json = tt.onEvent();
 		assertTrue(json != null);
 		assertEquals("true",(String)json.get("error"));
 		
