6ce0918d678ac27c0f95f78c566cc86d2547c728
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 13:39:00 -0700

    Updating GlobalsTerraTower with new fixes

--- src/edu/uci/ics/luci/TerraTower/GlobalsTerraTower.java
+++ src/edu/uci/ics/luci/TerraTower/GlobalsTerraTower.java
@@ -1,11 +1,11 @@
 /*
-	Copyright 2007-2014
+	Copyright 2014
 		University of California, Irvine (c/o Donald J. Patterson)
 */
 /*
-	This file is part of the Laboratory for Ubiquitous Computing java Utility package, i.e. "Utilities"
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
 
-    Utilities is free software: you can redistribute it and/or modify
+    TerraTower is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -18,7 +18,6 @@
     You should have received a copy of the GNU General Public License
     along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 package edu.uci.ics.luci.TerraTower;
 
 
