23189ffcc19ff677b9e13ea34321d5e60790b2b9
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 15:13:50 -0700

    Cleaning up post Place Tower event

--- src_test/edu/uci/ics/luci/TerraTower/events/TTEventCreateWorldTest.java
+++ src_test/edu/uci/ics/luci/TerraTower/events/TTEventCreateWorldTest.java
@@ -1,14 +1,33 @@
-package edu.uci.ics.luci.TerraTower.gameEvents;
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
 
-import static org.junit.Assert.*;
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.uci.ics.luci.TerraTower.events;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import edu.uci.ics.luci.TerraTower.gameEvents.TTEventCreateWorld;
 
 public class TTEventCreateWorldTest {
 
@@ -59,8 +78,17 @@ public class TTEventCreateWorldTest {
 		t2 = new TTEventCreateWorld(worldName,password+"x");
 		assertTrue(!t1.equals(t2));
 		
-		t2 = new TTEventCreateWorld(worldName,null);
-		t3 = new TTEventCreateWorld(worldName,null);
+		t2 = new TTEventCreateWorld(worldName,(String)null);
+		t3 = new TTEventCreateWorld(worldName,(String)null);
+		assertTrue(t2.equals(t3));
+		assertTrue(t2.hashCode()==t3.hashCode());
+		assertTrue(!t1.equals(t2));
+		assertTrue(t1.hashCode()!=t2.hashCode());
+		assertTrue(!t2.equals(t1));
+		assertTrue(t2.hashCode()!=t1.hashCode());
+		
+		t2 = new TTEventCreateWorld(worldName,(byte[])null);
+		t3 = new TTEventCreateWorld(worldName,(byte[])null);
 		assertTrue(t2.equals(t3));
 		assertTrue(t2.hashCode()==t3.hashCode());
 		assertTrue(!t1.equals(t2));
