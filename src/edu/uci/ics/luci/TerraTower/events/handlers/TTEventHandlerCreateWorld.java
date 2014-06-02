55cc9a9c5da1ac2066f65eb3d9051bf5775b7072
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 16:16:02 -0700

    And cleaning up post place tower event

--- src/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreateWorld.java
+++ src/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreateWorld.java
@@ -1,59 +1,59 @@
-package edu.uci.ics.luci.TerraTower.gameEvents;
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
+
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.uci.ics.luci.TerraTower.events.handlers;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import edu.uci.ics.luci.TerraTower.GlobalsTerraTower;
-import edu.uci.ics.luci.TerraTower.TTEvent;
-import edu.uci.ics.luci.TerraTower.TTEventHandler;
-import edu.uci.ics.luci.TerraTower.world.WorldManager;
+import edu.uci.ics.luci.TerraTower.events.TTEvent;
+import edu.uci.ics.luci.TerraTower.events.TTEventCreateWorld;
 
 
-public class TTEventHandlerCreateWorld implements TTEventHandler{    
+public class TTEventHandlerCreateWorld extends TTEventHandler{    
+	
+
+	private GlobalsTerraTower g = null;
+	private String worldName = null;
+	private byte[] worldHashedPassword = null;
 	
 	@Override
-	public JSONObject onEvent(TTEvent _event) {
+	public JSONObject checkParameters(long eventTime, TTEvent _event) {
+		//Don't Check parent because parent checks if world exists see TTEventHandlerPlaceTower for example
 		JSONObject ret = new JSONObject();
-		TTEventCreateWorld event = (TTEventCreateWorld) _event;
 		
-		GlobalsTerraTower globalsTerraTower = GlobalsTerraTower.getGlobalsTerraTower();
-		if(globalsTerraTower == null){
+		TTEventCreateWorld event = null;
+		if(_event instanceof TTEventCreateWorld){
+			event = ((TTEventCreateWorld) _event);
+		}
+		else{
 			ret.put("error","true");
 			JSONArray errors = new JSONArray();
-			errors.add("Globals wasn't initialized with GlobalsTerraTower");
+			errors.add("Internal error, event type mismatch\n"+this.getClass().getCanonicalName()+" was called with "+_event.getClass().getCanonicalName());
 			ret.put("errors", errors);
 			return ret;
 		}
-		WorldManager wm = globalsTerraTower.getWorldManager();
-		if(wm == null){
-			wm = new WorldManager();
-			globalsTerraTower.setWorldManager(new WorldManager());
-		}
-		
-		if(wm.worldExists(event.getName())){
-			ret.put("error","true");
-			JSONArray errors = new JSONArray();
-			errors.add("World with name, "+event.getName()+" already exists");
-			ret.put("errors", errors);
-		}else{
-			if(wm.create(event.getName(),event.getPassword())){
-				ret.put("error", "false");
-			}
-			else{
-				ret.put("error","true");
-				JSONArray errors = new JSONArray();
-				errors.add("World with name, "+event.getName()+" could not be created");
-				ret.put("errors", errors);
-			}
-		}
-		return ret;
-	}
 
-	@Override
-	public JSONObject checkParameters(TTEvent _event) {
-		JSONObject ret = new JSONObject();
-		TTEventCreateWorld event = (TTEventCreateWorld) _event;
-		
-		if(event.getName() == null){
+		//Make sure the world name is not null
+		worldName = event.getWorldName();
+		if(worldName == null){
 			ret.put("error","true");
 			JSONArray errors = new JSONArray();
 			errors.add("World can't have a null name");
@@ -61,14 +61,54 @@ public class TTEventHandlerCreateWorld implements TTEventHandler{
 			return ret;
 		}
 		
-		if(event.getPassword() == null){
+		//Make sure the world password is not null
+		worldHashedPassword = event.getWorldHashedPassword();
+		/*We know it's not null*/
+		/*
+		if(worldHashedPassword == null){
 			ret.put("error","true");
 			JSONArray errors = new JSONArray();
 			errors.add("World can't have a null password");
+			ret.put("errors", errors);
+			return ret;
+		}*/
+		
+		//Make sure globals is not null
+		g = GlobalsTerraTower.getGlobalsTerraTower();
+		if(g == null){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Global variables have not been initiazed");
+			ret.put("errors", errors);
+			return ret;
+		}
+		
+		//Make sure world doesn't exist
+		if(g.worldExists(event.getWorldName())){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("A world with name "+event.getWorldName()+" already exists");
 			ret.put("errors", errors);
 			return ret;
 		}
 		
 		return null;
 	}
+	
+	@Override
+	public JSONObject onEvent() {
+		JSONObject ret = new JSONObject();
+		
+		if(!g.createWorld(worldName, worldHashedPassword)){
+			ret.put("error","true");
+			JSONArray errors = new JSONArray();
+			errors.add("Unable to create a world for an unknown reason");
+			ret.put("errors", errors);
+			return ret;
+		}
+		
+		ret.put("error", "false");
+		return ret;
+	}
+
 }
--- src_test/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreateWorldTest.java
+++ src_test/edu/uci/ics/luci/TerraTower/events/handlers/TTEventHandlerCreateWorldTest.java
@@ -1,7 +1,27 @@
-package edu.uci.ics.luci.TerraTower.gameEvents;
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
 
-import static org.junit.Assert.*;
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package edu.uci.ics.luci.TerraTower.events.handlers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import net.minidev.json.JSONObject;
 
 import org.junit.After;
@@ -12,11 +32,12 @@ import org.junit.Test;
 
 import edu.uci.ics.luci.TerraTower.GlobalsTerraTower;
 import edu.uci.ics.luci.TerraTower.events.TTEventCreateWorld;
-import edu.uci.ics.luci.TerraTower.events.handlers.TTEventHandlerCreateWorld;
-import edu.uci.ics.luci.TerraTower.world.WorldManager;
+import edu.uci.ics.luci.TerraTower.events.TTEventVoid;
 import edu.uci.ics.luci.utility.Globals;
 
 public class TTEventHandlerCreateWorldTest {
+	final static String worldName = "name";
+	final static String worldPassword = "p"+System.currentTimeMillis();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -37,44 +58,59 @@ public class TTEventHandlerCreateWorldTest {
 	@Test
 	public void testParameters() {
 		TTEventHandlerCreateWorld tt = new TTEventHandlerCreateWorld();
-		String name = "name";
-		String password = "password";
-		TTEventCreateWorld event = new TTEventCreateWorld(name,password);
 		
-		JSONObject json = tt.checkParameters(event);
+		/* Wrong type*/
+		TTEventVoid tx = new TTEventVoid();
+		JSONObject json = tt.checkParameters(0,tx);
+		assertTrue(json != null);
+		assertEquals("true",(String)json.get("error"));
+		
+		/* Null world name*/
+		TTEventCreateWorld event = new TTEventCreateWorld(null,worldPassword);
+		json = tt.checkParameters(0,event);
+		assertTrue(json != null);
+		
+		/* Null globals */
+		event = new TTEventCreateWorld(worldName,worldPassword);
+		json = tt.checkParameters(0,event);
+		assertTrue(json != null);
+		
+		GlobalsTerraTower g = new GlobalsTerraTower("TEST_VERSION");
+		Globals.setGlobals(g);
+		
+		/* Ok */
+		event = new TTEventCreateWorld(worldName,worldPassword);
+		json = tt.checkParameters(0,event);
 		assertTrue(json == null);
 		
-		event = new TTEventCreateWorld(null,password);
-		json = tt.checkParameters(event);
-		assertTrue(json != null);
-		assertEquals("true",(String)json.get("error"));
+		g.createWorld(worldName, worldPassword);
 		
-		event = new TTEventCreateWorld(name,null);
-		json = tt.checkParameters(event);
+		/*World Exists */
+		event = new TTEventCreateWorld(worldName,worldPassword);
+		json = tt.checkParameters(0,event);
 		assertTrue(json != null);
-		assertEquals("true",(String)json.get("error"));
+		
 	}
 	
 	@Test
 	public void testOnEvent() {
-		GlobalsTerraTower g = new GlobalsTerraTower("Version test");
-		Globals.setGlobals(g);
-		g.setWorldManager(new WorldManager());
-		
 		TTEventHandlerCreateWorld tt = new TTEventHandlerCreateWorld();
-		String name = "name";
-		String password = "password";
-		TTEventCreateWorld event = new TTEventCreateWorld(name,password);
+
+		GlobalsTerraTower g = new GlobalsTerraTower("TEST_VERSION");
+		Globals.setGlobals(g);
 		
-		JSONObject json = tt.onEvent(event);
+		TTEventCreateWorld event = new TTEventCreateWorld(worldName,worldPassword);
+		JSONObject json = tt.checkParameters(0,event);
+		assertTrue(json == null);
+
+		json = tt.onEvent();
 		assertTrue(json != null);
 		assertEquals("false",(String)json.get("error"));
 		
-		json = tt.onEvent(event);
+		/* World already exists */
+		json = tt.onEvent();
 		assertTrue(json != null);
 		assertEquals("true",(String)json.get("error"));
-		
-		
 	}
 
 }
