100bfc1a02ec9a142306bf84ca99dca1f9fd5fab
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 15:43:26 -0700

    More cleanups post place tower event

--- src/edu/uci/ics/luci/TerraTower/events/TTEventPlayer.java
+++ src/edu/uci/ics/luci/TerraTower/events/TTEventPlayer.java
@@ -1,3 +1,23 @@
+/*
+	Copyright 2014
+		University of California, Irvine (c/o Donald J. Patterson)
+*/
+/*
+	This file is part of the Laboratory for Ubiquitous Computing java TerraTower game, i.e. "TerraTower"
+
+    TerraTower is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Utilities is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Utilities.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package edu.uci.ics.luci.TerraTower.events;
 
 import java.util.Arrays;
@@ -20,7 +40,12 @@ public class TTEventPlayer extends TTEvent {
 	}
 
 	public byte[] getPlayerHashedPassword() {
-		return Arrays.copyOf(playerHashedPassword,playerHashedPassword.length);
+		if(playerHashedPassword == null){
+			return new byte[0];
+		}
+		else{
+			return Arrays.copyOf(playerHashedPassword,playerHashedPassword.length);
+		}
 	}
 	
 	private void clearPlayerHashedPassword(){
