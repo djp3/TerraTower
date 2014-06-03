a40269b2337f155ad6b03682d2589dd425ba9268
Don Patterson <d_j_p_3@djp3.net>
2014-06-02 21:36:13 -0700

    Fixing up world manager post tower placing

---
+++ src_test/edu/uci/ics/luci/TerraTower/ResultChecker.java
@@ -0,0 +1,57 @@
+package edu.uci.ics.luci.TerraTower;
+
+import net.minidev.json.JSONObject;
+
+public class ResultChecker implements TTEventHandlerResultListener{
+	
+	private Object semaphore = new Object();
+	private Boolean resultOK = null;
+	private String expect;
+	private JSONObject results;
+	
+	public Object getSemaphore() {
+		return semaphore;
+	}
+	
+	public Boolean getResultOK() {
+		return resultOK;
+	}
+
+	public JSONObject getResults() {
+		return results;
+	}
+
+	ResultChecker(boolean expectError){
+		if(expectError){
+			this.expect = "true";
+		}
+		else{
+			this.expect = "false";
+		}
+	}
+
+	@Override
+	public void onFinish(JSONObject result) {
+		synchronized(semaphore){
+			results = result;
+			if(result == null){
+				resultOK = false;
+			}
+			else{
+				if(result.get("error") == null){
+					resultOK = false;
+				}
+				else{
+					if(result.get("error").equals(this.expect)){
+						resultOK = true;
+					}
+					else{
+						resultOK = false;
+					}
+				}
+			}
+			semaphore.notifyAll();
+		}
+	}
+	
+}
