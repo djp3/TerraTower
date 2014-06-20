4ca0fed23be6a74bd63e98478b762683c8e0cbf6
Don Patterson <d_j_p_3@djp3.net>
2014-06-20 15:40:51 -0700

    Checking in the Eclipse Android Project that was generated by the Unity Client

---
+++ UnityTerraTowerClient/EclipseProject/UnityTerraTowerClient/gen/com/northwoodgames/terratowerclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.northwoodgames.terratowerclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
